a1c1c831060d1b91f8729272478990a8f638520f
Daniil-Fuks <130780639+Daniil-Fuks@users.noreply.github.com>
2024-03-26 22:19:38 +0300

    Update main.py, create_dish.html
    Обновлена кнопка "Сохранить"

--- main.py
+++ main.py
@@ -165,6 +165,24 @@ def create_dish():
     else:
         return render_template('create_dish.html', form=form)
 
+@app.route('/dish_saved', methods=['POST'])
+def dish_saved():
+    return f"""<!doctype html>
+<html lang="en"> 
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Отбор астронавтов</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+  </head>
+  <body>
+  <div class="container">
+    <h1 class="text-center">Данные получены</h1>  
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+    </div>  
+  </body>
+</html>"""
+
 
 @app.route('/logout')
 @login_required
